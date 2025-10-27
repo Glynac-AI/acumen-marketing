f84db9c3609b07f663627bcaadb10d1ad5f2677b
Venu Boga <bogavenugopal96@gmail.com>
2025-10-27 18:17:26 +0530

    FRONT-16 Resolved refreshing issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
